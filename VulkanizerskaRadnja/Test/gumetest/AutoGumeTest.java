febc87be18e008d127912fd2a91df882d2bf4aa4
vidan997 <milojevic27116@fon.bg.ac.rs>
2018-03-16 15:58:07 +0100

    Napravljen test.

---
+++ VulkanizerskaRadnja/Test/gumetest/AutoGumeTest.java
@@ -0,0 +1,59 @@
+package gumetest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AutoGumeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetMarkaModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPrecnik() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSirina() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVisina() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}

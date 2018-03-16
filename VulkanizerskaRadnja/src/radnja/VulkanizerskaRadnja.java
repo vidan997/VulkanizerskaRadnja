d795df9f83c3eecd45f7a0b061a0c1e97e2028bf
vidan997 <milojevic27116@fon.bg.ac.rs>
2018-03-16 20:40:54 +0100

    Dodat opis za sve metode i atribute u klasi VulkanizerskaRadnja

--- VulkanizerskaRadnja/src/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/radnja/VulkanizerskaRadnja.java
@@ -3,11 +3,22 @@ package radnja;
 import java.util.LinkedList;
 
 import gume.AutoGuma;
-
+/**
+ * Klasa VulkanizerskaRadnja koja predstavlja rad.
+ * @author Vidan Milojevic
+ *
+ */
 public class VulkanizerskaRadnja {
-	
+	/**
+	 * Predstavlja istu guma koja radnja poseduje.
+	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
-
+	/**
+	 * Metoda koja dodaje novu gumu u listu
+	 * @param Objekat klase AutoGuma
+	 * @throws java.lang.NullPointerException kada je uneti objekat null.
+	 * @throws java.lang.RuntimeException kada uneti objekat vec postoji u listi.
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -15,7 +26,11 @@ public class VulkanizerskaRadnja {
 			throw new RuntimeException("Guma vec postoji");
 		gume.addFirst(a);
 	}
-
+	/**
+	 * Metoda koja vraca listu sa svim gumama od date marke
+	 * @param Marka gume
+	 * @return Lista sa gumama
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
